568a08e77dd9d3dbbecde3934c8fcd9fd33df787
Thorbjørn Steen <therubberduckmail@gmail.com>
2016-12-29 10:55:00 +0100

    Changed PlayerWindow size

--- HexAdventureMapper/PlayerWindow.Designer.cs
+++ HexAdventureMapper/PlayerWindow.Designer.cs
@@ -40,18 +40,19 @@
             this.btnMoveNorth1 = new System.Windows.Forms.Button();
             this.btnMoveEast2 = new System.Windows.Forms.Button();
             this.btnMoveWest1 = new System.Windows.Forms.Button();
-            this.imgPlayerMap = new HexAdventureMapper.Views.MapBox();
             this.imgLoadingIndicator = new System.Windows.Forms.PictureBox();
-            ((System.ComponentModel.ISupportInitialize)(this.imgPlayerMap)).BeginInit();
+            this.imgPlayerMap = new HexAdventureMapper.Views.MapBox();
             ((System.ComponentModel.ISupportInitialize)(this.imgLoadingIndicator)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.imgPlayerMap)).BeginInit();
             this.SuspendLayout();
             // 
             // btnMoveSouth3
             // 
             this.btnMoveSouth3.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
-            this.btnMoveSouth3.Location = new System.Drawing.Point(680, 577);
+            this.btnMoveSouth3.Location = new System.Drawing.Point(1020, 888);
+            this.btnMoveSouth3.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.btnMoveSouth3.Name = "btnMoveSouth3";
-            this.btnMoveSouth3.Size = new System.Drawing.Size(100, 30);
+            this.btnMoveSouth3.Size = new System.Drawing.Size(150, 46);
             this.btnMoveSouth3.TabIndex = 38;
             this.btnMoveSouth3.Text = "VVV";
             this.btnMoveSouth3.UseVisualStyleBackColor = true;
@@ -60,9 +61,10 @@
             // btnMoveSouth2
             // 
             this.btnMoveSouth2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
-            this.btnMoveSouth2.Location = new System.Drawing.Point(48, 577);
+            this.btnMoveSouth2.Location = new System.Drawing.Point(72, 888);
+            this.btnMoveSouth2.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.btnMoveSouth2.Name = "btnMoveSouth2";
-            this.btnMoveSouth2.Size = new System.Drawing.Size(100, 30);
+            this.btnMoveSouth2.Size = new System.Drawing.Size(150, 46);
             this.btnMoveSouth2.TabIndex = 37;
             this.btnMoveSouth2.Text = "VV";
             this.btnMoveSouth2.UseVisualStyleBackColor = true;
@@ -71,9 +73,10 @@
             // btnMoveNorth3
             // 
             this.btnMoveNorth3.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
-            this.btnMoveNorth3.Location = new System.Drawing.Point(680, 10);
+            this.btnMoveNorth3.Location = new System.Drawing.Point(1020, 15);
+            this.btnMoveNorth3.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.btnMoveNorth3.Name = "btnMoveNorth3";
-            this.btnMoveNorth3.Size = new System.Drawing.Size(100, 30);
+            this.btnMoveNorth3.Size = new System.Drawing.Size(150, 46);
             this.btnMoveNorth3.TabIndex = 36;
             this.btnMoveNorth3.Text = "^^^";
             this.btnMoveNorth3.UseVisualStyleBackColor = true;
@@ -81,9 +84,10 @@
             // 
             // btnMoveNorth2
             // 
-            this.btnMoveNorth2.Location = new System.Drawing.Point(48, 10);
+            this.btnMoveNorth2.Location = new System.Drawing.Point(72, 15);
+            this.btnMoveNorth2.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.btnMoveNorth2.Name = "btnMoveNorth2";
-            this.btnMoveNorth2.Size = new System.Drawing.Size(100, 30);
+            this.btnMoveNorth2.Size = new System.Drawing.Size(150, 46);
             this.btnMoveNorth2.TabIndex = 35;
             this.btnMoveNorth2.Text = "^^";
             this.btnMoveNorth2.UseVisualStyleBackColor = true;
@@ -92,9 +96,10 @@
             // btnMoveWest3
             // 
             this.btnMoveWest3.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
-            this.btnMoveWest3.Location = new System.Drawing.Point(12, 471);
+            this.btnMoveWest3.Location = new System.Drawing.Point(18, 725);
+            this.btnMoveWest3.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.btnMoveWest3.Name = "btnMoveWest3";
-            this.btnMoveWest3.Size = new System.Drawing.Size(30, 100);
+            this.btnMoveWest3.Size = new System.Drawing.Size(45, 154);
             this.btnMoveWest3.TabIndex = 34;
             this.btnMoveWest3.Text = "<\r\n<\r\n<";
             this.btnMoveWest3.UseVisualStyleBackColor = true;
@@ -102,9 +107,10 @@
             // 
             // btnMoveWest2
             // 
-            this.btnMoveWest2.Location = new System.Drawing.Point(12, 46);
+            this.btnMoveWest2.Location = new System.Drawing.Point(18, 71);
+            this.btnMoveWest2.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.btnMoveWest2.Name = "btnMoveWest2";
-            this.btnMoveWest2.Size = new System.Drawing.Size(30, 100);
+            this.btnMoveWest2.Size = new System.Drawing.Size(45, 154);
             this.btnMoveWest2.TabIndex = 33;
             this.btnMoveWest2.Text = "<\r\n<";
             this.btnMoveWest2.UseVisualStyleBackColor = true;
@@ -113,9 +119,10 @@
             // btnMoveEast3
             // 
             this.btnMoveEast3.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
-            this.btnMoveEast3.Location = new System.Drawing.Point(786, 471);
+            this.btnMoveEast3.Location = new System.Drawing.Point(1179, 725);
+            this.btnMoveEast3.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.btnMoveEast3.Name = "btnMoveEast3";
-            this.btnMoveEast3.Size = new System.Drawing.Size(30, 100);
+            this.btnMoveEast3.Size = new System.Drawing.Size(45, 154);
             this.btnMoveEast3.TabIndex = 32;
             this.btnMoveEast3.Text = ">\r\n>\r\n>";
             this.btnMoveEast3.UseVisualStyleBackColor = true;
@@ -125,9 +132,10 @@
             // 
             this.btnMoveEast1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
             | System.Windows.Forms.AnchorStyles.Right)));
-            this.btnMoveEast1.Location = new System.Drawing.Point(786, 152);
+            this.btnMoveEast1.Location = new System.Drawing.Point(1179, 234);
+            this.btnMoveEast1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.btnMoveEast1.Name = "btnMoveEast1";
-            this.btnMoveEast1.Size = new System.Drawing.Size(30, 313);
+            this.btnMoveEast1.Size = new System.Drawing.Size(45, 482);
             this.btnMoveEast1.TabIndex = 31;
             this.btnMoveEast1.Text = ">";
             this.btnMoveEast1.UseVisualStyleBackColor = true;
@@ -137,9 +145,10 @@
             // 
             this.btnMoveSouth1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
-            this.btnMoveSouth1.Location = new System.Drawing.Point(154, 577);
+            this.btnMoveSouth1.Location = new System.Drawing.Point(231, 888);
+            this.btnMoveSouth1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.btnMoveSouth1.Name = "btnMoveSouth1";
-            this.btnMoveSouth1.Size = new System.Drawing.Size(520, 30);
+            this.btnMoveSouth1.Size = new System.Drawing.Size(780, 46);
             this.btnMoveSouth1.TabIndex = 30;
             this.btnMoveSouth1.Text = "V";
             this.btnMoveSouth1.UseVisualStyleBackColor = true;
@@ -149,9 +158,10 @@
             // 
             this.btnMoveNorth1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
-            this.btnMoveNorth1.Location = new System.Drawing.Point(154, 10);
+            this.btnMoveNorth1.Location = new System.Drawing.Point(231, 15);
+            this.btnMoveNorth1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.btnMoveNorth1.Name = "btnMoveNorth1";
-            this.btnMoveNorth1.Size = new System.Drawing.Size(520, 30);
+            this.btnMoveNorth1.Size = new System.Drawing.Size(780, 46);
             this.btnMoveNorth1.TabIndex = 29;
             this.btnMoveNorth1.Text = "^";
             this.btnMoveNorth1.UseVisualStyleBackColor = true;
@@ -160,9 +170,10 @@
             // btnMoveEast2
             // 
             this.btnMoveEast2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
-            this.btnMoveEast2.Location = new System.Drawing.Point(786, 46);
+            this.btnMoveEast2.Location = new System.Drawing.Point(1179, 71);
+            this.btnMoveEast2.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.btnMoveEast2.Name = "btnMoveEast2";
-            this.btnMoveEast2.Size = new System.Drawing.Size(30, 100);
+            this.btnMoveEast2.Size = new System.Drawing.Size(45, 154);
             this.btnMoveEast2.TabIndex = 28;
             this.btnMoveEast2.Text = ">\r\n>";
             this.btnMoveEast2.UseVisualStyleBackColor = true;
@@ -172,13 +183,29 @@
             // 
             this.btnMoveWest1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
             | System.Windows.Forms.AnchorStyles.Left)));
-            this.btnMoveWest1.Location = new System.Drawing.Point(12, 152);
+            this.btnMoveWest1.Location = new System.Drawing.Point(18, 234);
+            this.btnMoveWest1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.btnMoveWest1.Name = "btnMoveWest1";
-            this.btnMoveWest1.Size = new System.Drawing.Size(30, 313);
+            this.btnMoveWest1.Size = new System.Drawing.Size(45, 482);
             this.btnMoveWest1.TabIndex = 27;
             this.btnMoveWest1.Text = "<";
             this.btnMoveWest1.UseVisualStyleBackColor = true;
             this.btnMoveWest1.Click += new System.EventHandler(this.btnMoveWest1_Click);
+            // 
+            // imgLoadingIndicator
+            // 
+            this.imgLoadingIndicator.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.imgLoadingIndicator.BackColor = System.Drawing.SystemColors.Control;
+            this.imgLoadingIndicator.Image = global::HexAdventureMapper.Properties.Resources.LoadingIndicator;
+            this.imgLoadingIndicator.Location = new System.Drawing.Point(1179, 15);
+            this.imgLoadingIndicator.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.imgLoadingIndicator.Name = "imgLoadingIndicator";
+            this.imgLoadingIndicator.Size = new System.Drawing.Size(45, 46);
+            this.imgLoadingIndicator.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
+            this.imgLoadingIndicator.TabIndex = 40;
+            this.imgLoadingIndicator.TabStop = false;
+            this.imgLoadingIndicator.UseWaitCursor = true;
+            this.imgLoadingIndicator.Visible = false;
             // 
             // imgPlayerMap
             // 
@@ -186,32 +213,19 @@
             | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
             this.imgPlayerMap.BackColor = System.Drawing.Color.DarkSeaGreen;
-            this.imgPlayerMap.Location = new System.Drawing.Point(48, 46);
+            this.imgPlayerMap.Location = new System.Drawing.Point(72, 71);
+            this.imgPlayerMap.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.imgPlayerMap.Name = "imgPlayerMap";
-            this.imgPlayerMap.Size = new System.Drawing.Size(732, 525);
+            this.imgPlayerMap.Size = new System.Drawing.Size(1098, 808);
             this.imgPlayerMap.TabIndex = 26;
             this.imgPlayerMap.TabStop = false;
             this.imgPlayerMap.MapClick += new HexAdventureMapper.Views.MapEventHandler(this.imgPlayerMap_Click);
             // 
-            // imgLoadingIndicator
-            // 
-            this.imgLoadingIndicator.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
-            this.imgLoadingIndicator.BackColor = System.Drawing.SystemColors.Control;
-            this.imgLoadingIndicator.Image = global::HexAdventureMapper.Properties.Resources.LoadingIndicator;
-            this.imgLoadingIndicator.Location = new System.Drawing.Point(786, 10);
-            this.imgLoadingIndicator.Name = "imgLoadingIndicator";
-            this.imgLoadingIndicator.Size = new System.Drawing.Size(30, 30);
-            this.imgLoadingIndicator.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
-            this.imgLoadingIndicator.TabIndex = 40;
-            this.imgLoadingIndicator.TabStop = false;
-            this.imgLoadingIndicator.UseWaitCursor = true;
-            this.imgLoadingIndicator.Visible = false;
-            // 
             // PlayerWindow
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(828, 619);
+            this.ClientSize = new System.Drawing.Size(1242, 952);
             this.Controls.Add(this.imgLoadingIndicator);
             this.Controls.Add(this.btnMoveSouth3);
             this.Controls.Add(this.btnMoveSouth2);
@@ -226,10 +240,11 @@
             this.Controls.Add(this.btnMoveNorth1);
             this.Controls.Add(this.btnMoveEast2);
             this.Controls.Add(this.btnMoveWest1);
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.Name = "PlayerWindow";
             this.Text = "PlayerWindow";
-            ((System.ComponentModel.ISupportInitialize)(this.imgPlayerMap)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.imgLoadingIndicator)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.imgPlayerMap)).EndInit();
             this.ResumeLayout(false);
 
         }
